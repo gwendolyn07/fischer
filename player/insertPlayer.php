71060a041e9f53a6a34cddf412ae8b46c32749e0
Gwendolyn <gwendolyn1107@gmail.com>
2017-11-28 11:48:39 -0600

    Fixed insert player

--- player/insertPlayer.php
+++ player/insertPlayer.php
@@ -1,33 +1,52 @@
-<?php
-	$servername = "localhost";
-	$username = "root";
-	$password = "Seth0426";
-	$dbname = "ur";
+		<?php
+			$servername = "localhost";
+			$username = "root";
+			$password = "Seth0426";
+			$dbname = "ur";
 
-	// Create Connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check Connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	} 
-	
-	$highSchool_Id= mysqli_real_escape_string($link, $_REQUEST['hsID]);
-	$player_Id= mysqli_real_escape_string($link, $_REQUEST['pID']);
-	$first_name= mysqli_real_escape_string($link, $_REQUEST['firstName']);
-	$last_name= mysqli_real_escape_string($link, $_REQUEST['lastName']);
-	$password= mysqli_real_escape_string($link, $_REQUEST['password']);
-	$DOB= mysqli_real_escape_string($link, $_REQUEST['dateOfBirth']);
-	$graduation_date= mysqli_real_escape_string($link, $_REQUEST['gradDate']);
-	$gender= mysqli_real_escape_string($link, $_REQUEST['gender']);
+			// Create Connection
+			$conn = new mysqli($servername, $username, $password, $dbname);
+			// Check Connection
+			if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+			} 
 
-	$sql = "INSERT INTO Player (pStudentID, pFName, pLName, pPassword, pDOB, pGraduationDate, highSchoolID, gender) VALUES ($player_Id, '$first_name', '$last_name', '$password', '$DOB', '$graduation_date', $highSchool_Id, '$gender')";
-	
-	if(mysqli_query($link, $sql)){
-		echo "Records added successfully.";
-	} else {
-		echo "ERROR: Not able to execute $sql. " .mysqli_error($link);
-	}
+			//set variables
+                        $first_name= mysqli_real_escape_string($conn, $_POST['firstName']);
+                        $last_name= mysqli_real_escape_string($conn, $_POST['lastName']);
+                        $password= mysqli_real_escape_string($conn, $_POST['password']);
+                        $DOB= mysqli_real_escape_string($conn, $_POST['DOB']);
+                        $graduation_date= mysqli_real_escape_string($conn, $_POST['gradDate']);
+			//set gender
+			if (isset($_POST['male'])) {
+				$gender = mysqli_real_escape_string($conn, "M");
+			}
+			if (isset($_POST['female'])) {
+				$gender = mysqli_real_escape_string($conn, "F");
+			}
 
-	mysqli_conn->close();
-?>
+			//check sports played
+                        if (isset($_POST['football'])) {
+                                $gender = mysqli_real_escape_string($conn, isset($_POST['football']));
+                        }
+                        if (isset($_POST['basketbll'])) {
+                                $gender = mysqli_real_escape_string($conn, "F");
+                        }
+
+
+			//sql statement
+                        $sql = "INSERT INTO Player (pStudentID, pFName, pLName, pPassword, pDOB, pGraduationDate, highSchoolID, gender) VALUES ('".$_POST[sID]."', '$first_name', '$last_name', '$password', '$DOB', '$graduation_date', '".$_POST[hsID]."' ,'$gender')";
+
+			//run statement
+			$result = $conn -> query($sql);
+
+			//check statement
+                        if ($result === TRUE) {
+                                echo "New record created successfully";
+                        } else {
+                                echo "Error: " . $sql . "<br>" . $conn->error;
+                        }
+
+			$conn->close();
+		?>
 
--- wtf.php
+++ wtf.php
@@ -1,6 +1,3 @@
-<!DOCTYPE html>
-<html>
-	<body>
 		<?php
 			$servername = "localhost";
 			$username = "root";
@@ -13,19 +10,28 @@
 			if ($conn->connect_error) {
 				die("Connection failed: " . $conn->connect_error);
 			} 
-			
-			$sql = "INSERT INTO Player (pStudentID, pFName, pLName, pPassword, pDOB, pGraduationDate, highSchoolID, gender) VALUES (678, 'Kwahi', 'Leonard', 'KL', '1989-09-09', '2020-09-09', 2 ,'M')";
+
+                        $highSchool_Id= mysqli_real_escape_string($conn, $_REQUEST[hsID]);
+                        $student_Id= mysqli_real_escape_string($conn, $_REQUEST[sID]);
+                        $first_name= mysqli_real_escape_string($conn, $_REQUEST['firstName']);
+                        $last_name= mysqli_real->escape_string($conn, $_REQUEST['lastName']);
+                        $password= mysqli_real_escape_string($conn, $_REQUEST['password']);
+                        $DOB= mysqli_real_escape_string($conn, $_REQUEST['dateOfBirth']);
+                        $graduation_date= mysqli_real_escape_string($conn, $_REQUEST['gradDate']);
+                        $gender= mysqli_real_escape_string($conn, $_REQUEST['gender']);
+
+
+                        $sql = "INSERT INTO Player (pStudentID, pFName, pLName, pPassword, pDOB, pGraduationDate, highSchoolID, gender) VALUES ($student_ID, '$first_name', '$last_name', '$password', '$DOB', '$graduation_date', highSchool_Id ,'$gender')";
+
 			$result = $conn -> query($sql);
 
-			if($result ->num_rows > 0){
-				echo "YAY!";
+			if ($result === TRUE) {
+    				echo "New record created successfully";
 			} else {
-				echo "0 results";
+    				echo "Error: " . $sql . "<br>" . $conn->error;
 			}
+
 
 			$conn->close();
 		?>
-
-	</body>
-</html>
 
